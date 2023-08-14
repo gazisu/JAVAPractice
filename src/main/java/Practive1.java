2e8d22f9973e6eec4909a699f4b2163fb18c68da
Shohel <Shohel@cel.local>
2023-08-14 17:14:09 +0600

    JAVA practive initial version 2

--- src/main/java/Practive1.java
+++
@@ -1,5 +0,0 @@
-public class Practive1 {
-    public static void main(String[] args) {
-        System.out.println("Practice Java");
-    }
-}
---
+++ src/test/java/Practive1.java
@@ -0,0 +1,17 @@
+import com.google.common.annotations.VisibleForTesting;
+import org.testng.annotations.Test;
+
+public class Practive1 {
+    public static void main(String[] args) {
+
+
+        System.out.println("Process \nfinished \nwith \n exit \n code");
+        System.out.println("Process \tfinished \twith \t exit \t code");
+        System.out.println(" \"Process finished with exit code\"");
+
+
+                System.out.println(" \"Process finished with exit code\"");
+
+
+    }
+}
